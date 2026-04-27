13e193565e27211d1b91a28dbf5e29b9d482ce10
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-04-27 18:01:14 +0900

    :wrench: Type cloudfront_base_url as URI::HTTPS
    Validates at startup that the value is an HTTPS URL rather than
    accepting any string, catching misconfiguration early

--- config/settings.rb
+++ config/settings.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+require "uri"
+
 module PastaAtlas
   class Settings < Hanami::Settings
     setting :s3_bucket, constructor: Types::String
-    setting :cloudfront_base_url, constructor: Types::String
+    setting :cloudfront_base_url, constructor: ->(v) {
+      uri = URI(v.to_s)
+      raise Dry::Types::CoercionError, "cloudfront_base_url must be an HTTPS URL" unless uri.is_a?(URI::HTTPS)
+
+      uri
+    }
     setting :presigned_url_expiry, default: 3600, constructor: Types::Params::Integer
   end
 end
