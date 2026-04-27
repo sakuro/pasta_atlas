491789b253bdc2e0f8be57d245e7aed1acd05081
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-04-27 15:35:01 +0900

    :sparkles: Include Dry::Monads[:result] in Operation base class
    Consistent with Action; makes Success/Failure explicitly available
    in all operation subclasses for result handling

--- app/operation.rb
+++ app/operation.rb
@@ -1,9 +1,11 @@
 # auto_register: false
 # frozen_string_literal: true
 
+require "dry/monads"
 require "dry/operation"
 
 module PastaAtlas
   class Operation < Dry::Operation
+    include Dry::Monads[:result]
   end
 end
