acefca8520c88da276c7896fed7ed43d39e218d1
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-05-09 12:38:35 +0900

    :sparkles: Add i18n helpers to view context
    - locale_tag: returns current locale BCP47 tag from rack env
    - locale_name: returns ICU4X DisplayNames-based locale display name
    - t: translates a message key (supports dot notation for attributes)

--- app/views/context.rb
+++ app/views/context.rb
@@ -1,12 +1,45 @@
 # auto_register: false
 # frozen_string_literal: true
 
+require "rack/icu4x/locale"
 require "rack/protection"
 
 module PastaAtlas
   module Views
     class Context < Hanami::View::Context
       include Deps["repos.user_repo", "repos.user_profile_repo", "settings"]
+
+      def initialize(i18n: nil, **args)
+        super(**args)
+        @i18n = i18n
+      end
+
+      attr_reader :i18n
+
+      def locale_tag
+        locales = request&.env&.[](Rack::ICU4X::Locale::ENV_KEY)
+        locales&.first&.to_s || "en"
+      end
+
+      def locale_name(locale_code) = ICU4X::DisplayNames.new(ICU4X::Locale.parse(locale_tag), type: :language).of(locale_code.to_s)
+
+      def t(key, **args)
+        return key.to_s unless i18n
+
+        id, attr = key.to_s.split(".", 2)
+        unless attr
+          return i18n.format(id, **args)
+        end
+
+        bundle = i18n.find(id)
+        return key.to_s unless bundle
+
+        message = bundle.message(id)
+        attr_pattern = message&.attributes&.[](attr)
+        return key.to_s unless attr_pattern
+
+        bundle.format_pattern(attr_pattern, nil, **args)
+      end
 
       def current_user_name
         user_id = session[:user_id]
