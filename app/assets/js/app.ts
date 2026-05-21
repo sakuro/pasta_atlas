3f5ade25ec4df5fd85180bbf80a27219d5fcb309
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-05-22 01:24:15 +0900

    :sparkles: Add close button to layout flash notifications

--- app/assets/js/app.ts
+++ app/assets/js/app.ts
@@ -4,6 +4,12 @@ import "@fortawesome/fontawesome-free/css/solid.css";
 import "@fortawesome/fontawesome-free/css/brands.css";
 import "../css/app.css";
 
+document.querySelectorAll<HTMLElement>(".notification > .delete").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.closest(".notification")?.parentElement?.remove();
+  });
+});
+
 document.querySelectorAll<HTMLElement>(".navbar-item.has-dropdown").forEach((dropdown) => {
   dropdown.querySelector(".navbar-link")?.addEventListener("click", () => {
     dropdown.classList.toggle("is-active");
