795e536a05cc2e2345ba1d372aced142261c7e9d
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-05-23 20:45:44 +0900

    :sparkles: Add alternate timestamp format as tooltip on hover
    - Add localize_absolute_datetime and localize_relative_datetime as public
      context methods so templates can request either format explicitly
    - Show absolute time as tooltip when relative timestamps are enabled,
      and relative time as tooltip when absolute timestamps are shown
    - Apply to both map cards (server-rendered) and MapViewer (client-side)

--- app/views/context.rb
+++ app/views/context.rb
@@ -32,9 +32,29 @@ module PastaAtlas
       end
 
       def localize_datetime(time)
-        return localize_relative_time(time) if viewer_relative_timestamps
+        viewer_relative_timestamps ? localize_relative_datetime(time) : localize_absolute_datetime(time)
+      end
 
+      def localize_absolute_datetime(time)
         Foxtail::Function::DateTime[time.utc, {timeZone: viewer_timezone, dateStyle: :medium, timeStyle: :short}]
+      end
+
+      def localize_relative_datetime(time)
+        locale = ICU4X::Locale.parse(locale_tag)
+        formatter = ICU4X::RelativeTimeFormat.new(locale, style: :long, numeric: :auto)
+        diff = Integer(Time.now.utc - time.utc)
+        abs = diff.abs
+        unit =
+          if abs < 60 then :second
+          elsif abs < 3600 then :minute
+          elsif abs < 86_400 then :hour
+          elsif abs < 7 * 86_400 then :day
+          elsif abs < 30 * 86_400 then :week
+          elsif abs < 365 * 86_400 then :month
+          else :year
+          end
+        divisor = {second: 1, minute: 60, hour: 3600, day: 86_400, week: 7 * 86_400, month: 30 * 86_400, year: 365 * 86_400}[unit]
+        formatter.format(-(diff / divisor), unit)
       end
 
       def t(key, **args)
@@ -91,24 +111,6 @@ module PastaAtlas
       end
 
       private def viewer_timezone = viewer_preference&.timezone || "UTC"
-
-      private def localize_relative_time(time)
-        locale = ICU4X::Locale.parse(locale_tag)
-        formatter = ICU4X::RelativeTimeFormat.new(locale, style: :long, numeric: :auto)
-        diff = Integer(Time.now.utc - time.utc)
-        abs = diff.abs
-        unit =
-          if abs < 60 then :second
-          elsif abs < 3600 then :minute
-          elsif abs < 86_400 then :hour
-          elsif abs < 7 * 86_400 then :day
-          elsif abs < 30 * 86_400 then :week
-          elsif abs < 365 * 86_400 then :month
-          else :year
-          end
-        divisor = {second: 1, minute: 60, hour: 3600, day: 86_400, week: 7 * 86_400, month: 30 * 86_400, year: 365 * 86_400}[unit]
-        formatter.format(-(diff / divisor), unit)
-      end
     end
   end
 end
