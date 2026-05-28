524429feecbf68c1c87fd8b3b409fc4281a0bd38
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-05-28 11:48:19 +0900

    :bug: Scroll only mod list in map info modal
    - Wrap mod table in a scrollable div (max-height: 300px)
    - Prevents the entire modal from growing tall when many mods are listed

--- frontend/components/MapInfoModal.tsx
+++ frontend/components/MapInfoModal.tsx
@@ -131,18 +131,20 @@ export const MapInfoModal = (props: { mapshot: Mapshot; onClose: () => void }) =
                           data-l10n-id="map-info-mods-count"
                           data-l10n-args={JSON.stringify({ count: Object.keys(props.mapshot.active_mods!).length })}
                         />
-                        <table class="table is-narrow is-fullwidth">
-                          <tbody>
-                            <For each={sortedMods(props.mapshot.active_mods!)}>
-                              {([name, version]) => (
-                                <tr>
-                                  <td><a href={`https://mods.factorio.com/mod/${name}`} target="_blank" rel="noopener">{name}</a></td>
-                                  <td>{version}</td>
-                                </tr>
-                              )}
-                            </For>
-                          </tbody>
-                        </table>
+                        <div style={{ "overflow-y": "auto", "max-height": "300px" }}>
+                          <table class="table is-narrow is-fullwidth">
+                            <tbody>
+                              <For each={sortedMods(props.mapshot.active_mods!)}>
+                                {([name, version]) => (
+                                  <tr>
+                                    <td><a href={`https://mods.factorio.com/mod/${name}`} target="_blank" rel="noopener">{name}</a></td>
+                                    <td>{version}</td>
+                                  </tr>
+                                )}
+                              </For>
+                            </tbody>
+                          </table>
+                        </div>
                       </details>
                     </td>
                   </tr>
