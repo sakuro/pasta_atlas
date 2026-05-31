c6baf097639953e518b81988b7d16f95552a9172
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-06-01 00:04:44 +0900

    :bug: Close NavAuthMenu dropdown after selecting an item
    - Replace is-hoverable with click-controlled is-active on UserMenu
    - Close dropdown on menu item click to prevent it staying open after SPA navigation
    - Add document click listener for outside-click-to-close

--- frontend/components/NavAuthMenu.tsx
+++ frontend/components/NavAuthMenu.tsx
@@ -1,4 +1,4 @@
-import { Show } from "solid-js";
+import { Show, createSignal, onCleanup } from "solid-js";
 import { Avatar } from "./Avatar";
 import "../lib/l10n";
 
@@ -14,41 +14,64 @@ type Props = {
 
 type UserMenuProps = Omit<Props, "omniauthToken">;
 
-const UserMenu = (props: UserMenuProps) => (
-  <div class="navbar-item has-dropdown is-hoverable">
-    <a class="navbar-link">
-      <Avatar url={props.avatarUrl || null} size={24} />
-      <span class="ml-2">{props.displayName}</span>
-    </a>
-    <div class="navbar-dropdown is-right">
-      <a class="navbar-item" href={`${props.userPath}#tab-recent-maps`}>
-        <span class="icon"><i class="fa-solid fa-map" /></span>
-        <span data-l10n-id="user-tab-maps" />
+const UserMenu = (props: UserMenuProps) => {
+  const [isOpen, setIsOpen] = createSignal(false);
+  let containerRef: HTMLDivElement | undefined;
+
+  const close = () => setIsOpen(false);
+
+  const handleDocumentClick = (e: MouseEvent) => {
+    if (containerRef && !containerRef.contains(e.target as Node)) {
+      close();
+    }
+  };
+
+  const toggle = () => {
+    setIsOpen(v => {
+      if (!v) document.addEventListener("click", handleDocumentClick);
+      else document.removeEventListener("click", handleDocumentClick);
+      return !v;
+    });
+  };
+
+  onCleanup(() => document.removeEventListener("click", handleDocumentClick));
+
+  return (
+    <div ref={containerRef} class={`navbar-item has-dropdown${isOpen() ? " is-active" : ""}`}>
+      <a class="navbar-link" onClick={toggle}>
+        <Avatar url={props.avatarUrl || null} size={24} />
+        <span class="ml-2">{props.displayName}</span>
       </a>
-      <a class="navbar-item" href={`${props.userPath}#tab-profile`}>
-        <span class="icon"><i class="fa-solid fa-circle-user" /></span>
-        <span data-l10n-id="user-tab-profile" />
-      </a>
-      <a class="navbar-item" href={`${props.userPath}#tab-preferences`}>
-        <span class="icon"><i class="fa-solid fa-gear" /></span>
-        <span data-l10n-id="user-tab-preferences" />
-      </a>
-      <a class="navbar-item" href={`${props.userPath}#tab-credentials`}>
-        <span class="icon"><i class="fa-solid fa-link" /></span>
-        <span data-l10n-id="user-tab-credentials" />
-      </a>
-      <hr class="navbar-divider" />
-      <form action={props.logoutPath} method="post">
-        <input type="hidden" name="_method" value="delete" />
-        <input type="hidden" name="_csrf_token" value={props.csrfToken} />
-        <button class="navbar-item" type="submit">
-          <span class="icon"><i class="fa-solid fa-arrow-right-from-bracket" /></span>
-          <span data-l10n-id="nav-log-out" />
-        </button>
-      </form>
+      <div class="navbar-dropdown is-right">
+        <a class="navbar-item" href={`${props.userPath}#tab-recent-maps`} onClick={close}>
+          <span class="icon"><i class="fa-solid fa-map" /></span>
+          <span data-l10n-id="user-tab-maps" />
+        </a>
+        <a class="navbar-item" href={`${props.userPath}#tab-profile`} onClick={close}>
+          <span class="icon"><i class="fa-solid fa-circle-user" /></span>
+          <span data-l10n-id="user-tab-profile" />
+        </a>
+        <a class="navbar-item" href={`${props.userPath}#tab-preferences`} onClick={close}>
+          <span class="icon"><i class="fa-solid fa-gear" /></span>
+          <span data-l10n-id="user-tab-preferences" />
+        </a>
+        <a class="navbar-item" href={`${props.userPath}#tab-credentials`} onClick={close}>
+          <span class="icon"><i class="fa-solid fa-link" /></span>
+          <span data-l10n-id="user-tab-credentials" />
+        </a>
+        <hr class="navbar-divider" />
+        <form action={props.logoutPath} method="post">
+          <input type="hidden" name="_method" value="delete" />
+          <input type="hidden" name="_csrf_token" value={props.csrfToken} />
+          <button class="navbar-item" type="submit">
+            <span class="icon"><i class="fa-solid fa-arrow-right-from-bracket" /></span>
+            <span data-l10n-id="nav-log-out" />
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const LoginMenu = (props: { omniauthToken: string }) => (
   <div class="navbar-item has-dropdown is-hoverable">
