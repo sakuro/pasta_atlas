13f18aff75505678c04659923fb43cbfa7755e44
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-06-04 19:49:54 +0900

    :art: Move Generation method definitions into initial class body

--- app/structs/generation.rb
+++ app/structs/generation.rb
@@ -3,9 +3,6 @@
 module PastaAtlas
   module Structs
     class Generation < PastaAtlas::DB::Struct
-    end
-
-    class Generation
       def metadata_url(base_url) = "#{base_url}/#{metadata_s3_key}"
       def thumbnail_url(base_url) = "#{base_url}/#{metadata_s3_key.sub("mapshot.json", "s1zoom_4/tile_0_0.jpg")}"
     end
