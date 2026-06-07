726f10732fd59698b653c0becb08fcebdd4b3288
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-06-07 13:26:32 +0900

    :lock: Restrict ALB ingress to CloudFront managed prefix list
    - Replace open 0.0.0.0/0 and ::/0 ingress rules with a single rule
      scoped to the CloudFront origin-facing managed prefix list
    - Drop HTTPS ingress rules since CloudFront connects to the ALB over HTTP only
    - Prevents direct access to the ALB bypassing CloudFront

--- terraform/environments/production/alb.tf
+++ terraform/environments/production/alb.tf
@@ -1,38 +1,18 @@
+data "aws_ec2_managed_prefix_list" "cloudfront" {
+  name = "com.amazonaws.global.cloudfront.origin-facing"
+}
+
 resource "aws_security_group" "alb" {
   name        = "${var.app_name}-${var.environment}-alb"
-  description = "Allow HTTP/HTTPS from internet"
+  description = "Allow HTTP from CloudFront only"
   vpc_id      = data.aws_vpc.default.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+resource "aws_vpc_security_group_ingress_rule" "alb_http_from_cloudfront" {
   security_group_id = aws_security_group.alb.id
-  cidr_ipv4         = "0.0.0.0/0"
+  prefix_list_id    = data.aws_ec2_managed_prefix_list.cloudfront.id
   from_port         = 80
   to_port           = 80
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "alb_http_ipv6" {
-  security_group_id = aws_security_group.alb.id
-  cidr_ipv6         = "::/0"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "alb_https" {
-  security_group_id = aws_security_group.alb.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "alb_https_ipv6" {
-  security_group_id = aws_security_group.alb.id
-  cidr_ipv6         = "::/0"
-  from_port         = 443
-  to_port           = 443
   ip_protocol       = "tcp"
 }
 
